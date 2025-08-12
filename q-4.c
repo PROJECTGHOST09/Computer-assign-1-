fc7b9634063244959ccf60f7b71fdd4477f0d422
Gohil Jitrajsinh <projectghost15@gmail.com>
2025-08-12 22:01:43 +0530

    home laptop

---
+++ q-4.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{ 
+ printf("Enter two numbers for subtraction : ");
+ int a,b,c ;
+ scanf("%d %d", &a, &b);
+ c = a - b;
+ printf("The subtraction of two no.  is : %d", c);
+ return 0;
+}
