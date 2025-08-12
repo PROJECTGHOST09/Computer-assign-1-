151c1a9fe4df090e091bf8a24024354098715201
Gohil Jitrajsinh <projectghost15@gmail.com>
2025-08-12 22:12:49 +0530

    home laptop

---
+++ q-7.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    printf("Changing minutes to hour \n");
+    printf("Enter the minutes you  want to convert ");
+    float m ;
+    scanf("%f",&m);
+    m=m/60;
+    printf("The time in hours is : %f",m);
+
+}
