d659c9b719654655e7998396734cf5f04ffa60e0
Gohil Jitrajsinh <projectghost15@gmail.com>
2025-08-13 22:03:59 +0530

    dollatopoutoinr.c

--- dollatopoutoinr.c
+++ dollatopoutoinr.c
@@ -6,8 +6,9 @@ int main()
     printf("Enter amount in inr : ");
     float i,d,p;
     scanf("%f",&i);
-    d=i/48;
-    p=d*1.45833;
+    d=i*48;
+    p=d*1.45833; // as 1 dollar is equal to 48 inr and i pound is equal to 70 inr so ny solving in inr we get 1 inr=(d/48)=(p/70)
     printf("Amount in pounds (converted from dollar): %f \n", p);
     return 0;
 }
+
