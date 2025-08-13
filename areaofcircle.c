c71603f81f6efa3b7091086b5ad8d8d0f0ec66b0
Gohil Jitrajsinh <projectghost15@gmail.com>
2025-08-13 21:12:55 +0530

    home

---
+++ areaofcircle.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    printf("To find the area of circle \n");
+    printf("Enter the radius :");
+    float r,a;
+    scanf("%f",&r);
+    a=(22/7)*r*r;
+    printf("Area is : %f\n",a);
+    
+}
